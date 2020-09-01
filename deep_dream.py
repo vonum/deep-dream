e81ac168dcbfc2c91abb0904db04b0618e1674b0
vonum <vonum.mk@gmail.com>
2020-09-01 17:43:52 +0200

    Reinjecting details

--- deep_dream.py
+++ deep_dream.py
@@ -46,18 +46,46 @@ class DeepDream:
     return DeepDream.deprocess_image(image)
 
   def run_deep_dream_for_octaves(self, image, steps=200, step_size=0.01,
-                                 octave_range=range(-2, 3), octave_scale=1.30, callback=None):
+                                 octave_range=range(-2, 3), octave_scale=1.30,
+                                 reinject_details=False, original_image=None, callback=None):
     image = tf.constant(np.array(image))
     base_shape = tf.shape(image)[:-1]
     float_base_shape = tf.cast(base_shape, tf.float32)
 
-    for n in octave_range:
-      new_shape = tf.cast(float_base_shape * (octave_scale**n), tf.int32)
+    if reinject_details:
+      original_image = tf.constant(np.array(original_image))
+      original_image_shape = tf.shape(original_image)[:-1]
+      float_original_image_shape = tf.cast(original_image_shape, tf.float32)
 
-      image = tf.image.resize(image, new_shape).numpy()
+      lowest_octave = octave_range[0]
+      shrunk_original_image_shape = tf.cast(
+        float_original_image_shape * (octave_scale**lowest_octave),
+        tf.int32
+      )
+      shrunk_original_image = tf.image.resize(original_image,
+                                              shrunk_original_image_shape).numpy()
+
+    for n in octave_range:
+      shape = tf.cast(float_base_shape * (octave_scale**n), tf.int32)
+
+      image = tf.image.resize(image, shape).numpy()
 
       image = self.run_deep_dream(image, steps=steps, step_size=step_size, callback=callback)
 
+      if reinject_details:
+        image = self._reinject_details(image, original_image, shrunk_original_image, shape)
+
+        shrunk_original_image = tf.image.resize(original_image, shape).numpy()
+
+    return image
+
+  def _reinject_details(self, image, original_image, shrunk_original_image, shape):
+    upscaled_shrunk_original_image = tf.image.resize(shrunk_original_image, shape).numpy()
+    same_size_original_image = tf.image.resize(original_image, shape).numpy()
+    lost_detail = same_size_original_image - upscaled_shrunk_original_image
+
+    image += lost_detail
+    # shrunk_original_img = resize_img(original_img, shape)
     return image
 
   @staticmethod
